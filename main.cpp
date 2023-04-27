0bd34facdb18616b5044e92d0c5559da3eafebc1
Foxunderground0 <umerirfan1205@gmail.com>
2023-04-27 23:28:46 +0100

    Added Linux terminal support to detect keypresses and move the player accordingly

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <math.h>
-#include <SDL2/SDL.h>
+#include <iostream>
+#include <termios.h>
+#include <unistd.h>
 
 #define mapWidth 8
 #define mapHeight 8
@@ -10,96 +12,114 @@ using namespace std;
 
 double posX = ((double) mapWidth/2.0)-0.52 ,posY= (double) mapHeight/2.0,phy=0.0;
 
-int map[8][8]={{1,1,1,1,1,1,1,1}
-				,{1,0,0,0,0,0,0,1}
-				,{1,0,0,1,0,0,0,1}
-				,{1,0,0,0,0,0,0,1}
-				,{1,0,0,0,0,0,0,1}
-				,{1,0,0,1,0,0,0,1}
-				,{1,0,0,0,0,0,0,1}
-				,{1,1,1,1,1,1,1,1}};
+int kbhit() {
+	struct timeval tv = { 0L, 0L };
+	fd_set fds;
+	FD_ZERO(&fds);
+	FD_SET(STDIN_FILENO, &fds);
+	return select(STDIN_FILENO + 1, &fds, NULL, NULL, &tv) == 1;
+}
 
+int map[8][8]={{1,1,1,1,1,1,1,1}
+	,{1,0,0,0,0,0,0,1}
+	,{1,0,0,1,0,0,0,1}
+	,{1,0,0,0,0,0,0,1}
+	,{1,0,0,0,0,0,0,1}
+	,{1,0,0,1,0,0,0,1}
+	,{1,0,0,0,0,0,0,1}
+	,{1,1,1,1,1,1,1,1}};
 
 bool gridHasPlayer(int x, int y){
-				if(((int)posX == x) && ((int)posY == y)){
-								return true;
-				}
-				return false;
+	x = mapWidth - x;
+	//y = mapHeight - y;
+
+	if(((int)posX == x) && ((int)posY == y)){
+		return true;
+	}
+	return false;
+}
+
+void getInput(){
+	char ch = getchar();
+	// Check for simple WASD key presses
+	if (ch == 'w') {
+		cout << "Moving up" << endl;
+		posX += 0.1;
+	} else if (ch == 'a') {
+		cout << "Moving left" << endl;
+		posY -= 0.1;
+	} else if (ch == 's') {
+		cout << "Moving down" << endl;
+		posX -= 0.1;
+	} else if (ch == 'd') {
+		cout << "Moving right" << endl;
+		posY += 0.1;
+	} else if (ch == 'q') {
+		//break;
+	}
 }
 
 void drawPlayer(){
-				//playe
-				double highOrLow = posX - (double)((int)posX);
+	//playe
+	double highOrLow = posY - (double)((int)posY);
 
-				if(highOrLow >= 0.5){
-								cout << " " << "⣿";
-				} else {
-								cout << "⣿" << " ";
-				}
+	if(highOrLow >= 0.5){
+		cout << " " << "⣿";
+	} else {
+		cout << "⣿" << " ";
+	}
 }
 
 void renderBuffer(){
-				char displayBuffer[8][16]={" "};
+	char displayBuffer[8][16]={" "};
 
-				//Draw map on displayBuffer
-				for(int _ = 0; _ < 8; _++){
-								for(int __ = 0; __ < 8; __++){
-												displayBuffer[_][__] = map[_][__];
-								}
-								cout << endl;
+	//Draw map on displayBuffer
+	for(int _ = 0; _ < 8; _++){
+		for(int __ = 0; __ < 8; __++){
+			displayBuffer[_][__] = map[_][__];
+		}
+		cout << endl;
+	}
+
+	//Draw player
+	//displayBuffer[posX][posY] = "⣿";
+
+	//Draw displayBuffer
+	for(int _ = 0; _ < 8; _++){
+		for(int __ = 0; __ < 16; __++){
+			if(!gridHasPlayer(_,__) || (_ >= 8)){
+				if (displayBuffer[_][__]){
+					cout << "██";
+				} else {
+					cout << "  ";
 				}
-
-				//Draw player
-				//displayBuffer[posX][posY] = "⣿";
-
-				//Draw displayBuffer
-				for(int _ = 0; _ < 8; _++){
-								for(int __ = 0; __ < 16; __++){
-												if(!gridHasPlayer(_,__) || (_ >= 8)){
-																if (displayBuffer[_][__]){
-																				cout << "██";
-																} else {
-																				cout << "  ";
-																}
-												} else {
-																drawPlayer();
-												}
-								}
-								cout << endl;
-				}
-				cout << "-----------------------" << endl;
-				cout << "posX: " << posX << " poxY: " << posY;
+			} else {
+				drawPlayer();
+			}
+		}
+		cout << endl;
+	}
+	cout << "-----------------------" << endl;
+	cout << "posX: " << posX << " poxY: " << posY;
 
 }
 
-int main(int argc, char* argv[]) {
-    SDL_Init(SDL_INIT_EVENTS);
-
-    //SDL_Window* window = SDL_CreateWindow("Hello World", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
-
-    bool running = true;
-    while (running) {
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_SPACE) {
-                std::cout << "Hello World!" << std::endl;
-            }
-        }
-        SDL_Delay(10);
-    }
-
-    //SDL_DestroyWindow(window);
-    SDL_Quit();
-
-    return 0;
-}
-
-/*
 int main(){
-				
-				//while(1){
-				renderBuffer();
-				//}
+	struct termios oldt, newt;
+	tcgetattr(STDIN_FILENO, &oldt);
+	newt = oldt;
+	newt.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
 
-				return 0;
-} */
+
+
+	while(1){
+		if(kbhit()){
+			renderBuffer();
+			getInput();
+		}
+	}
+
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+	return 0;
+}
